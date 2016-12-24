06f59099a5cf7312c9ec34ed4b0a93f89ed04727
katherinelazar <catzar5@gmail.com>
2016-12-24 09:58:18 -0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,5 @@
 // Put your game functions/methods in here.
 // Think about when you want your functions to be called (on DOM event, on page load, etc.)
+var turn = 0;
+var symbol = 'X';
+var player = 'X';
