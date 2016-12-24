000da36aab44622091056116ec65472d1b1367e4
katherinelazar <catzar5@gmail.com>
2016-12-24 11:41:47 -0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,51 @@ var symbol = 'X';
 var player = 'X';
 function check()
 {
+  if ($('#1').innerHTML() === symbol && $('#2').innerHTML() === symbol && $('#3').innerHTML() === symbol ) 
+  {
+    $('#1, #2, #3').className = 'won';
+    alert(player + ' won!');
+  } 
+  else if ($('#4').innerHTML() === symbol && $('#5').innerHTML() === symbol && $('#6').innerHTML() === symbol ) 
+  {
+    $('#4, #5, #6').className = 'won';
+    alert(player + ' won!');
+  } 
+  else if ($('#7').innerHTML() === symbol && $('#8').innerHTML() === symbol && $('#9').innerHTML() === symbol ) 
+  {
+    $('#7, #8, #9').className = 'won';
+    alert(player + ' won!');
+  } 
+  else if ($('#7').innerHTML() === symbol && $('#4').innerHTML() === symbol && $('#1').innerHTML() === symbol ) 
+  {
+    $('#7, #4, #1').className = 'won';
+    alert(player + ' won!');
+  } 
+  else if ($('#8').innerHTML() === symbol && $('#5').innerHTML() === symbol && $('#2').innerHTML() === symbol ) 
+  {
+    $('#8, #5, #2').className = 'won';
+    alert(player + ' won!');
+  } 
+  else if ($('#9').innerHTML() === symbol && $('#6').innerHTML() === symbol && $('#3').innerHTML() === symbol ) 
+  {
+    $('#9, #6, #3').className = 'won';
+    alert(player + ' won!');
+  } 
+  else if ($('#7').innerHTML() === symbol && $('#5').innerHTML() === symbol && $('#3').innerHTML() === symbol ) 
+  {
+    $('#7, #5, #3').className = 'won';
+    alert(player + ' won!');
+  } 
+  else if ($('#9').innerHTML() === symbol && $('#5').innerHTML() === symbol && $('#1').innerHTML() === symbol ) 
+  {
+    $('#9, #5, #1').className = 'won';
+    alert(player + ' won!');
+  }
+  
+  else if (turn === 8) 
+  {
+    alert("You tied!")
+  }
   )
 
   $('#restart').click(function()
