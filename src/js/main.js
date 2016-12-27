267de481f5e7561567901dd5ed6dacbbcf4e88c7
katherinelazar <catzar5@gmail.com>
2016-12-26 18:58:06 -0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,61 +1,64 @@
 // Put your game functions/methods in here.
 // Think about when you want your functions to be called (on DOM event, on page load, etc.)
-var turn = 0;
-var symbol = 'X';
-var player = 'X';
-function check()
-{
-  if ($('#1').innerHTML() === symbol && $('#2').innerHTML() === symbol && $('#3').innerHTML() === symbol ) 
-  {
-    $('#1, #2, #3').className = 'won';
-    alert(player + ' won!');
-  } 
-  else if ($('#4').innerHTML() === symbol && $('#5').innerHTML() === symbol && $('#6').innerHTML() === symbol ) 
-  {
-    $('#4, #5, #6').className = 'won';
-    alert(player + ' won!');
-  } 
-  else if ($('#7').innerHTML() === symbol && $('#8').innerHTML() === symbol && $('#9').innerHTML() === symbol ) 
-  {
-    $('#7, #8, #9').className = 'won';
-    alert(player + ' won!');
-  } 
-  else if ($('#7').innerHTML() === symbol && $('#4').innerHTML() === symbol && $('#1').innerHTML() === symbol ) 
-  {
-    $('#7, #4, #1').className = 'won';
-    alert(player + ' won!');
-  } 
-  else if ($('#8').innerHTML() === symbol && $('#5').innerHTML() === symbol && $('#2').innerHTML() === symbol ) 
-  {
-    $('#8, #5, #2').className = 'won';
-    alert(player + ' won!');
-  } 
-  else if ($('#9').innerHTML() === symbol && $('#6').innerHTML() === symbol && $('#3').innerHTML() === symbol ) 
-  {
-    $('#9, #6, #3').className = 'won';
-    alert(player + ' won!');
-  } 
-  else if ($('#7').innerHTML() === symbol && $('#5').innerHTML() === symbol && $('#3').innerHTML() === symbol ) 
-  {
-    $('#7, #5, #3').className = 'won';
-    alert(player + ' won!');
-  } 
-  else if ($('#9').innerHTML() === symbol && $('#5').innerHTML() === symbol && $('#1').innerHTML() === symbol ) 
-  {
-    $('#9, #5, #1').className = 'won';
-    alert(player + ' won!');
-  }
-  
-  else if (turn === 8) 
-  {
-    alert("You tied!")
-  }
-  )
+var GRID_SIZE = 3;
+var grid = [];
+var player = 1;
 
-  $('#restart').click(function()
+for (var i = 0; i < GRID_SIZE; i++) 
 {
-  $('.square').innerHTML('');
-  $('.square').className = ""; 
-  // document.getElementById("whatever").className = "";
-  turn = 0;
+  grid[i] = [];
+  
+  for (var j = 0; j < GRID_SIZE; j++) 
+  {
+    grid[i][j] = 0;
+  }
+}
+
+var table = document.getElementById("tic-tac-toe-1");
+table.addEventListener("click", tableClick);
+
+function tableClick(event) 
+{
+    var element = event.target;
+    if (element && element.tagName === "TD") 
+    {
+       var y = element.cellIndex;
+       var x = element.parent.rowIndex;
+
+       clickCell(element, x, y);
+    }
+}
+
+function clickCell(cell, x, y) 
+{
+  if (grid[x][y] == 0) 
+  {
+    if (player == 1) 
+    {
+      cell.className = "x";
+      cell.innerHTML = "X";
+      grid[x][y]=1;
+      player = 2;
+    } 
+    else 
+    {
+      cell.className = "o";
+      cell.innerHTML = "O";
+      grid[x][y] = 2;
+      player = 1;
+    }
+  }
+}
+
+function reset() 
+{
+  for (var i = 0; i < GRID_SIZE; i++) 
+  {
+    for (var j = 0; j < GRID_SIZE; j++) 
+    {
+      grid[i][j]=0;
+      document.getElementById("cell_"+i+"_"+j).innerHTML = "&nbsp;";
+    }
+  }
+  player = 1;
 }
